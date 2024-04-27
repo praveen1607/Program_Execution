ef5428e3740077b1971697c8202e21c40998f39d
prave <prave@LAPTOP-GNP6TGRG>
2024-04-27 10:47:11 +0530

    Add project files.

---
+++ Program_Execution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_Execution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void One()
+        {
+            Console.WriteLine("I am Method One");
+        }
+    }
+}
---
+++ Program_Execution/ProgramOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_Execution
+{
+    public class ProgramOne
+    {
+        public static void Main(String[] args)   // It gives error saying it have more than one entry point
+        {
+            Program Pobj = new Program();
+            Pobj.One();
+        }
+
+    }
+}
